86261f77bca89f14208c53d300393e8a211189d4
EmaaZmaker <emaazbinammar@gmail.com>
2025-07-17 11:44:42 +0400

    dewjck

---
+++ activity3.py
@@ -0,0 +1,7 @@
+country_code={"India":"0091",
+              "australia":"0025",
+             "Nepal":"0097"}
+print ("country code for india")
+print (country_code.get("India","not found"))
+print ("country code for Al Ain")
+print (country_code.get("Al Ain","not found"))
